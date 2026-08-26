ca6642201a86b32ce6a01ae79063a8046279d2f3
Abhishek <abhishek98.social@gmail.com>
2026-08-26 17:42:24 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+
+  output: 'export',
+  basePath: '/portfolio',
+  trailingSlash: true,
+
   images: {
+    unoptimized: true,
     formats: ["image/avif", "image/webp"],
     domains: ["images.isbndb.com", "m.media-amazon.com"],
   },
+
   async redirects() {
     return [
       {
